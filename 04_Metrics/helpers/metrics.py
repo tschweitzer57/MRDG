e8e646ec3228ec89657182d183a310c2f6086600
tschweitzer57 <thibault.schweitzer@estaca.eu>
2025-02-14 16:44:41 +0100

    improved metrics

--- 04_Metrics/helpers/metrics.py
+++ 04_Metrics/helpers/metrics.py
@@ -79,20 +79,58 @@ class Metrics():
         else:
             raise ValueError("Unknown pose_relation argument")
 
-    def compute_rpe_rmse(self):
-        data = (self.traj_gt, self.traj_est)
-        rpe_metric = metrics.RPE(pose_relation=self.pose_relation, delta=self.rpe_settings.delta, delta_unit=self.rpe_settings.delta_unit, all_pairs=self.rpe_settings.all_pairs)
-        rpe_metric.process_data(data)
-        rpe_stat = rpe_metric.get_statistic(metrics.StatisticsType.rmse)
-        print('RPE: ',rpe_stat)
+    def __statisticsType(self, err_type):
+        if err_type == 'max':
+            return metrics.StatisticsType.max
+        elif err_type == 'min':
+            return metrics.StatisticsType.min
+        elif err_type == 'mean':
+            return metrics.StatisticsType.mean
+        elif err_type == 'median':
+            return metrics.StatisticsType.median
+        elif err_type == 'rmse':
+            return metrics.StatisticsType.rmse
+        elif err_type == 'sse':
+            return metrics.StatisticsType.sse
+        elif err_type == 'std':
+            return metrics.StatisticsType.std
+        else:
+            raise ValueError("Unknown statistics type argument")
 
-    def compute_ape_rmse(self):
+    def compute_rpe(self, err_type=None):
         data = (self.traj_gt, self.traj_est)
-        ape_metric = metrics.APE(self.pose_relation)
-        ape_metric.process_data(data)
-        ape_stat = ape_metric.get_statistic(metrics.StatisticsType.rmse)
-        print('APE: ',ape_stat)
+        self.rpe_metric = metrics.RPE(pose_relation=self.pose_relation, delta=self.rpe_settings.delta, delta_unit=self.rpe_settings.delta_unit, all_pairs=self.rpe_settings.all_pairs)
+        self.rpe_metric.process_data(data)
+        if err_type is not None:
+            stat_type = self.__statisticsType(err_type)
+            rpe_stat = self.rpe_metric.get_statistic(stat_type)
+            print('RPE [',err_type,'] :',rpe_stat)
+        
+
+    def compute_ape(self, err_type=None):
+        data = (self.traj_gt, self.traj_est)
+        self.ape_metric = metrics.APE(self.pose_relation)
+        self.ape_metric.process_data(data)
+        if err_type is not None:
+            stat_type = self.__statisticsType(err_type)
+            ape_stat = self.ape_metric.get_statistic(stat_type)
+            print('APE [',err_type,'] :',ape_stat)
 
     def compute_stats(self):
-        rpe_stats = rpe_metric.get_all_statistics()
-        ape_stats = rpe_metric.get_all_statistics()
+        self.compute_rpe()
+        self.compute_ape()
+        self.rpe_stats = self.rpe_metric.get_all_statistics()
+        self.ape_stats = self.ape_metric.get_all_statistics()
+        print('RPE: ', self.rpe_stats)
+        print('APE: ', self.ape_stats)
+
+    # def export(self, path):
+    #     print(path)
+    #     for
+    #     print()
+
+        #os.makedirs(raw_path, exist_ok=True)
+
+    # Error numbers
+    # print(self.ape_metric.error)
+    # print(self.rpe_metric.error)
--- 04_Metrics/scripts/get-results.py
+++ 04_Metrics/scripts/get-results.py
@@ -5,5 +5,5 @@ gt_path = './input/landmarks_d/d/groundtruth.txt'
 
 metrics = metrics.Metrics(est_path, gt_path)
 metrics.set_poseRelation('translation')
-metrics.compute_ape_rmse()
-metrics.compute_rpe_rmse()
+metrics.compute_stats()
+
