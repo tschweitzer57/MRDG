7313df8d1aa8093e0273a7b22d051e36171bbdc5
tschweitzer57 <thibault.schweitzer@estaca.eu>
2025-02-14 14:02:40 +0100

    added code to compute results

--- 04_Metrics/helpers/metrics.py
+++ 04_Metrics/helpers/metrics.py
@@ -78,3 +78,21 @@ class Metrics():
             self.pose_relation = metrics.PoseRelation.point_distance
         else:
             raise ValueError("Unknown pose_relation argument")
+
+    def compute_rpe_rmse(self):
+        data = (self.traj_gt, self.traj_est)
+        rpe_metric = metrics.RPE(pose_relation=self.pose_relation, delta=self.rpe_settings.delta, delta_unit=self.rpe_settings.delta_unit, all_pairs=self.rpe_settings.all_pairs)
+        rpe_metric.process_data(data)
+        rpe_stat = rpe_metric.get_statistic(metrics.StatisticsType.rmse)
+        print('RPE: ',rpe_stat)
+
+    def compute_ape_rmse(self):
+        data = (self.traj_gt, self.traj_est)
+        ape_metric = metrics.APE(self.pose_relation)
+        ape_metric.process_data(data)
+        ape_stat = ape_metric.get_statistic(metrics.StatisticsType.rmse)
+        print('APE: ',ape_stat)
+
+    def compute_stats(self):
+        rpe_stats = rpe_metric.get_all_statistics()
+        ape_stats = rpe_metric.get_all_statistics()
--- 04_Metrics/scripts/get-results.py
+++ 04_Metrics/scripts/get-results.py
@@ -1,6 +1,9 @@
 import metrics
 
-est_path = './test/landmarks_d/a/estimates.txt'
-gt_path = './test/landmarks_d/a/groundtruth.txt'
+est_path = './input/landmarks_d/d/estimates.txt'
+gt_path = './input/landmarks_d/d/groundtruth.txt'
 
 metrics = metrics.Metrics(est_path, gt_path)
+metrics.set_poseRelation('translation')
+metrics.compute_ape_rmse()
+metrics.compute_rpe_rmse()
