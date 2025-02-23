cecb524af91dbaa7dd31d81c33c2b393d0756199
tschweitzer57 <thibault.schweitzer@estaca.eu>
2025-02-23 03:22:38 +0100

    upgraded devcontainer

---
+++ 05_Display/scripts/test_display.py
@@ -0,0 +1,8 @@
+import metrics
+
+est_path = './input/landmarks_d/d/estimates.txt'
+gt_path = './input/landmarks_d/d/groundtruth.txt'
+
+metrics = metrics.Metrics(est_path, gt_path)
+metrics.set_poseRelation('translation')
+metrics.compute_stats()
