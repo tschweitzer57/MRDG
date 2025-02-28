761ed07f23bdf6c04da7b6e43823798edebff4e4
tschweitzer57 <thibault.schweitzer@estaca.eu>
2025-02-28 12:05:33 +0100

    added all functions to generate folder components

--- 03_Metrics/scripts/test.py
+++ 03_Metrics/scripts/test.py
@@ -1,4 +1,9 @@
 from metrics import Results
 
 test = Results('./input/landmarks_geodesic-mesa_2025-02-13_09-21-15', './saved_output', './datasets/landmarks.jrl')
+
+# Generate results folder
+test.generate_summary_file()
 test.generate_intermediate_results()
+test.generate_raw_errors_file()
+test.generate_metrics_results()
