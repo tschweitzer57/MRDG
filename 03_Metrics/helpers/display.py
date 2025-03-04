9bf18c636b5e3cf21c34e5bafb6ec9caff05bd2e
tschweitzer57 <thibault.schweitzer@estaca.eu>
2025-03-04 17:51:47 +0100

    changed matplotlib styles

--- 03_Metrics/helpers/display.py
+++ 03_Metrics/helpers/display.py
@@ -114,7 +114,28 @@ class Test():
     
 
     def boxplot(self, err_type=None):
-        print('not implemented')
+        style = ['Solarize_Light2', '_classic_test_patch', '_mpl-gallery', '_mpl-gallery-nogrid', 'bmh', 'classic', 'dark_background', 'fast', 'fivethirtyeight', 'ggplot', 'grayscale', 'seaborn-v0_8', 'seaborn-v0_8-bright', 'seaborn-v0_8-colorblind', 'seaborn-v0_8-dark', 'seaborn-v0_8-dark-palette', 'seaborn-v0_8-darkgrid', 'seaborn-v0_8-deep', 'seaborn-v0_8-muted', 'seaborn-v0_8-notebook', 'seaborn-v0_8-paper', 'seaborn-v0_8-pastel', 'seaborn-v0_8-poster', 'seaborn-v0_8-talk', 'seaborn-v0_8-ticks', 'seaborn-v0_8-white', 'seaborn-v0_8-whitegrid', 'tableau-colorblind10']
+        #plt.style.use('_mpl-gallery')
+        plt.style.use(style[4])
+
+        # make data:
+        np.random.seed(10)
+        D = np.random.normal((3, 5, 4), (1.25, 1.00, 1.25), (100, 3))
+
+        # plot
+        fig, ax = plt.subplots()
+        VP = ax.boxplot(D, positions=[2, 4, 6], widths=1.5, patch_artist=True,
+                        showmeans=False, showfliers=False,
+                        medianprops={"color": "white", "linewidth": 0.5},
+                        boxprops={"facecolor": "C0", "edgecolor": "white",
+                                "linewidth": 0.5},
+                        whiskerprops={"color": "C0", "linewidth": 1.5},
+                        capprops={"color": "C0", "linewidth": 1.5})
+
+        ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+            ylim=(0, 8), yticks=np.arange(1, 8))
+
+        plt.show()
 
     def errorbar(self, err_type=None):
         print('not implemented')
