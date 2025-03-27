631423808c6e1cf03863e9a2853b692356fd8b2e
tschweitzer57 <thibault.schweitzer@estaca.eu>
2025-03-27 06:33:04 +0100

    .

--- 01_Generator/scripts/gen-dataset_iccad.py
+++ 01_Generator/scripts/gen-dataset_iccad.py
@@ -104,9 +104,9 @@ def get_available_comms(vals, robots, pose_index, dist_thresh):
 if __name__ == "__main__":
 
     # Setup the Dataset Builder
-    input_dir = './configs/VAR'
-    output_dir = './saved_outputs/var'
-    config_name = 'default10'
+    input_dir = './configs/VAR_odom'
+    output_dir = './saved_outputs/var_odom'
+    config_name = 'odom9'
     builder = DatasetGenerator(os.path.join(input_dir, config_name + ".json"))
     if builder.params.lc_inter_direct is not None:
         if builder.params.lc_inter_direct.get('range') is not None:
