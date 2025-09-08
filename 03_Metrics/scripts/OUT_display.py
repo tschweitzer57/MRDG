0c8493277d21922064854e4b0b34357e4c2ae7ca
tschweitzer57 <thibault.schweitzer@estaca.eu>
2025-09-08 12:46:20 +0200

    mise à jour du script pour les résultats

--- 03_Metrics/scripts/OUT_display.py
+++ 03_Metrics/scripts/OUT_display.py
@@ -43,12 +43,11 @@ def get_data_paths(solvers, results_folder, dataset_folder):
 ###           BR_1 - Pyxis                      ###
 #####################################################
 dataset_folder = './datasets/OUT_V2'
-results_folder = './input/OUT_V2'
+results_folder = './input/OUT_V2_cluster2'
 output_folder = 'saved_output'
 display_folder = 'display_output'
 
-#solvers = ['pyxis-5', 'pyxis-6', 'pyxis-7']
-solvers = ['pyxis-7']
+solvers = ['pyxis-5', 'pyxis-6', 'pyxis-7']
 
 results_paths = get_data_paths(solvers, results_folder, dataset_folder)
 data = {}
