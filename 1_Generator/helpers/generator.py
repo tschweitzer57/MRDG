9270d9eea7d67ef2891e00d7a71641eceea4657b
tschweitzer57 <thibault.schweitzer@estaca.eu>
2026-01-01 19:31:07 +0100

    ajout de commentaires pour des foctions

--- 1_Generator/helpers/generator.py
+++ 1_Generator/helpers/generator.py
@@ -16,6 +16,10 @@ from configuration import DatasetConfiguration
 #TODO améliorer intégration des random seed
 
 class DatasetGenerator(jrl.DatasetBuilder):
+    """
+    Class DatasetGenerator
+    """
+
     def __init__(self, config_path):
 
         # Get configuration from config file
@@ -107,6 +111,13 @@ class DatasetGenerator(jrl.DatasetBuilder):
             return True
 
     def get_closest_lk(self, rid, pose_number):
+        """
+        Get the closest landmark to a given robot pose
+        
+        :param rid: Robot ID
+        :param pose_number: Pose number
+        :return: Key of the closest landmark
+        """
         r_key = gtsam.symbol(rid, pose_number)
         r_pose = self.gt_poses.atPose3(r_key).translation()
         first_lk = True
@@ -169,6 +180,12 @@ class DatasetGenerator(jrl.DatasetBuilder):
 
     # Generator for groundtruth trajectories
     def gen_gt_trajectories(self, nb_poses=None):
+        """
+        Generate groundtruth trajectories for each robot
+        
+        :param nb_poses: Number of poses to generate (if None, use config file value)
+        :return: None
+        """
 
         # Define number of poses
         if nb_poses is None:
@@ -219,6 +236,11 @@ class DatasetGenerator(jrl.DatasetBuilder):
     
     # Generator for landmarks amers
     def gen_lk_amers(self, nb=None):
+        """
+        Generate landmarks
+
+        :param nb: Number of landmarks to generate (if None, use config file value)
+        """
 
         if nb is None and self.config.landmarks is not None:
             nb = self.config.landmarks['number']
