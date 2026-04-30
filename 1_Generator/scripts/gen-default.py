70c9c67501b55bb10aaa6a3d1262d43af17e9af3
tschweitzer57 <thibault.schweitzer@estaca.eu>
2026-04-30 17:58:31 +0200

    ajout script défault

---
+++ 1_Generator/scripts/gen-default.py
@@ -0,0 +1,15 @@
+from generator import DatasetGenerator
+from configuration import get_config_paths
+
+if __name__ == "__main__":
+
+    # config_folder = './configs/VLD1'
+    # config_paths = get_config_paths(config_folder)
+
+    # for file_path in config_paths:
+    #     dataset = DatasetGenerator(file_path)
+    #     dataset.generate_dataset()
+
+    file_path = './configs/VLD1/mesa_test.json'
+    dataset = DatasetGenerator(file_path)
+    dataset.generate_dataset()
